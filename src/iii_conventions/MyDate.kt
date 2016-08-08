32f23a03168bd9f06e4bb8e6785d1eda6a85d79a
Denny Crall <dennis-crall@uiowa.edu>
2016-08-08 16:26:45 -0500

    Complete 25-28

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,19 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+    operator override fun compareTo(other: MyDate): Int {
+        val yearComparison = year.compareTo(other.year)
+        val monthComparison = month.compareTo(other.month)
+        val dayComparison = dayOfMonth.compareTo(other.dayOfMonth)
+        val comparison = listOf(yearComparison, monthComparison, dayComparison)
+        val result = comparison.firstOrNull() { it != 0 }
+        return result?: 0
+    }
+}
+
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +21,29 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+
+    operator override fun iterator() : Iterator<MyDate> {
+        return DateRangeIterator(start, endInclusive);
+    }
+
+    private class DateRangeIterator(val start: MyDate, val endInclusive: MyDate) : Iterator<MyDate> {
+
+        var current : MyDate = start
+
+        override fun next() : MyDate {
+            if(hasNext()) {
+                var next = current;
+                current = current.nextDay();
+                return next;
+            } else {
+                throw IllegalStateException()
+            }
+        }
+
+        override fun hasNext() : Boolean {
+            return current <= endInclusive;
+        }
+    }
+}
+
